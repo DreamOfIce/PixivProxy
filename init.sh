28d3416201271692867b1071d0c3cbfc820b64d1
creeper2077 <mccreeper2077@gmail.com>
2021-10-21 11:28:01 +0800

    尝试修复错误

--- init.sh
+++ init.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #Init
 
-pixivDomain = $PIXIV_DOMAIN
-pximgDomain = $PXIMG_DOMAIN
-if [-z $pixivDomain || -z $pximgDomain]
+pixivDomain=$PIXIV_DOMAIN
+pximgDomain=$PXIMG_DOMAIN
+if [ -z $pixivDomain || -z $pximgDomain ]
 then
     echo "[ERR]Cannot read the env!Have you set them?"
     echo "[INFO]For more Information,visit https://"
     exit 1
 fi
-pixivDomain2 = ${pixivDomain//"."/"\."}
-pximgDomain2 = ${pximgDomain//"."/"\."}
+pixivDomain2=${pixivDomain//"."/"\."}
+pximgDomain2=${pximgDomain//"."/"\."}
 
 echo "Pixiv Proxy"
 echo "Github: https://github.com/Creeper2077/pixiv-proxy-cn"
 echo " Using GPL3.0 License"
 echo "Please abide by the use agreement of relevant service providers!"
-printf "*.pixiv.net ==> *.%s *.pximg.net ==> *.%s" pixivDomain pximgDomain
+printf "*.pixiv.net ==> *.%s *.pximg.net ==> *.%s" $pixivDomain $pximgDomain
 
 echo "Replace the domain..."
 sed -i 's/@PIXIV_DOMAIN@/${pixivDomain}/g' /etc/nginx/nginx.conf
@@ -25,4 +25,4 @@ sed -i 's/@PXIMG_DOMAIN@/${pximgDomain}/g' /etc/nginx/nginx.conf
 sed -i 's/@PXIMG_DOMAIN2@/${pximgDomain2}/g' /etc/nginx/nginx.conf
 echo "Done."
 echo "Start nginx..."
-service nginx Start
+service nginx start
