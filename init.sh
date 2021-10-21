bc63fa9336f21d6b8cc02dd378f5734b810a9664
creeper2077 <mccreeper2077@gmail.com>
2021-10-21 11:43:53 +0800

    再一次尝试修复错误

--- init.sh
+++ init.sh
@@ -3,14 +3,14 @@
 
 pixivDomain=$PIXIV_DOMAIN
 pximgDomain=$PXIMG_DOMAIN
-if [ -z $pixivDomain || -z $pximgDomain ]
+if [ -z "$pixivDomain" -o -z "$pximgDomain" ]
 then
     echo "[ERR]Cannot read the env!Have you set them?"
     echo "[INFO]For more Information,visit https://"
     exit 1
 fi
-pixivDomain2=${pixivDomain//"."/"\."}
-pximgDomain2=${pximgDomain//"."/"\."}
+pixivDomain2=${pixivDomain//'.'/'\.'}
+pximgDomain2=${pximgDomain//'.'/'\.'}
 
 echo "Pixiv Proxy"
 echo "Github: https://github.com/Creeper2077/pixiv-proxy-cn"
@@ -26,3 +26,11 @@ sed -i 's/@PXIMG_DOMAIN2@/${pximgDomain2}/g' /etc/nginx/nginx.conf
 echo "Done."
 echo "Start nginx..."
 service nginx start
+if [ $? != 0 ]
+then
+    echo "[ERR]Start nginx failed!"
+    echo "[INFO]Please check your setting!"
+    exit 1
+else
+        exit 0
+fi
