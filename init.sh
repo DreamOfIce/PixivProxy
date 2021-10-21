63c989a77925501d8b5a859d2ba3b3853894f9d6
creeper2077 <mccreeper2077@gmail.com>
2021-10-21 10:50:17 +0800

    upload

---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Init
+
+pixivDomain = $PIXIV_DOMAIN
+pximgDomain = $PXIMG_DOMAIN
+if [-z $pixivDomain || -z $pximgDomain]
+then
+    echo "[ERR]Cannot read the env!Have you set them?"
+    echo "[INFO]For more Information,visit https://"
+    exit 1
+fi
+pixivDomain2 = ${pixivDomain//"."/"\."}
+pximgDomain2 = ${pximgDomain//"."/"\."}
+
+echo "Pixiv Proxy"
+echo "Github: https://github.com/Creeper2077/pixiv-proxy-cn"
+echo " Using GPL3.0 License"
+echo "Please abide by the use agreement of relevant service providers!"
+printf "*.pixiv.net ==> *.%s *.pximg.net ==> *.%s"
+
+echo "Replace the domain..."
+sed -i 's/@PIXIV_DOMAIN@/${pixivDomain}/g' /etc/nginx/nginx.conf
+sed -i 's/@PIXIV_DOMAIN2@/${pixiivDomain2}/g' /etc/nginx/nginx.conf
+sed -i 's/@PXIMG_DOMAIN@/${pximgDomain}/g' /etc/nginx/nginx.conf
+sed -i 's/@PXIMG_DOMAIN2@/${pximgDomain2}/g' /etc/nginx/nginx.conf
+echo "Done."
+echo "Start nginx..."
+service nginx Start
+exit 0
